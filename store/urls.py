39fa387f2f3beadab0a842f775fe3ede1b106b21
Nitin Gupta <iamvamsi999@gmail.com>
2023-04-17 22:59:49 +0530

    APIView

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail),
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:id>/', views.ProductDetails.as_view()),
     path('collection/', views.collection_list),
     path('collection/<int:pk>/', views.collection_detail, name='collection-detail'),
 ]
