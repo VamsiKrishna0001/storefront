8086bc157a2bcfc9439c3057b88c22232201c4c9
Nitin Gupta <iamvamsi999@gmail.com>
2023-04-18 21:44:50 +0530

    new apis

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
+from rest_framework_nested import routers
 from . import views
 
+
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collection', views.CollectionViewSet)
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetails.as_view()),
-    path('collection/', views.CollectionList.as_view()),
-    path('collection/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
-]
+urlpatterns = router.urls + products_router.urls
+# urlpatterns = [
+#     # path('products/', views.ProductList.as_view()),
+#     # path('products/<int:pk>/', views.ProductDetails.as_view()),
+#     # path('collection/', views.CollectionList.as_view()),
+#     # path('collection/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
+# ]
